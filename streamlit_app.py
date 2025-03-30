e6afbee6d25c63b99581ec605046dc7916a54160
paulodavism <paulodavism@gmail.com>
2025-03-30 08:10:11 -0300

    Atualiza configuração do driver para suportar Chromium no Linux

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,19 @@
 import streamlit as st
 from seleniumbase import Driver
+import platform
 
 """
 ## Web scraping on Streamlit Cloud with Selenium
 
 [![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
 
-Este é um exemplo de como fazer web scraping usando SeleniumBase com Chrome no Streamlit.
+Este é um exemplo de como fazer web scraping usando SeleniumBase com Chrome/Chromium no Streamlit.
 """
 
 @st.cache_resource
 def get_driver():
+    if platform.system() == "Linux":
+        return Driver(uc=True, headless=True, browser="chromium")
     return Driver(uc=True, headless=True)
 
 try:
